d71a70b2a7f923dcc8faf8f10b3c2943e05d3659
Matthias Alzate <matthias.alzate@elev.ga.ntig.se>
2024-11-07 13:13:01 +0100

    oiyhuioy

---
+++ noseOne/Program.cs
@@ -0,0 +1,35 @@
+﻿string name;
+string correctName = "kalleanka";
+string answer;
+int code;
+int correctCode = 12345;
+int i;
+
+if (6 >= 3 ) {
+    Console.WriteLine("Hello World!\n");
+}
+
+Console.WriteLine("Skriv in ditt användarnamn: ");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Skriv in kod: ");
+
+answer = Console.ReadLine();
+
+int.TryParse(answer, out code);
+
+if (name.ToLower() == correctName && code == correctCode) {
+    Console.WriteLine("Välkommen!");
+}
+else {
+    Console.WriteLine("Fel användarnamn eller kod\n");
+}
+
+for (i = 0; i < 32; i++) {
+    Console.WriteLine("Hello World!");
+}
+
+
+
+Console.ReadLine();
